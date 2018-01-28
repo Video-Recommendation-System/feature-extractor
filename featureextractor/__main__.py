ba64716984a10e41669838947a6ee9afff54948d
Christopher Wells <cwellsny@nycap.rr.com>
2018-01-27 20:18:02 -0500

    Implement classification grabbing

--- featureextractor/__main__.py
+++ featureextractor/__main__.py
@@ -14,21 +14,36 @@ def main():
     client = language.LanguageServiceClient()
 
     sentiments = []
-    for text in data["description"]:
-        sentiments.append(get_data(client, text))
+    classifications = []
+    descriptions = data["description"]
+    titles = data["video_title"]
+    for x in range(0, len(descriptions)):
+        text = descriptions.iloc[x]
+        print titles[x]
+
+        document = types.Document(
+            content = text,
+            type = enums.Document.Type.PLAIN_TEXT)
+        sentiments.append(get_sentiment(client, document))
+        categories = []
+        for category in get_classification(client, document).categories:
+            categories.append((category.name.encode("utf-8"), category.confidence))
+        classifications.append(categories)
 
     data["sentiment"] = sentiments
+    data["classifications"] = classifications
 
     print data
     data.to_csv(output_file, index=False)
 
 
-def get_data(client, text):
-    document = types.Document(
-        content = text,
-        type = enums.Document.Type.PLAIN_TEXT)
+def get_sentiment(client, document):
+    a = client.analyze_sentiment(document=document)
 
-    return client.analyze_sentiment(document=document).document_sentiment.magnitude
+    return a.document_sentiment.magnitude
+
+def get_classification(client, document):
+    return client.classify_text(document)
 
 if __name__ == "__main__":
     main()
