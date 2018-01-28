b289ec9f1fb825202bc9de870108985df9452e31
Christopher Wells <cwellsny@nycap.rr.com>
2018-01-27 19:10:01 -0500

    Implement sentiment magnitude grabbing

--- featureextractor/__main__.py
+++ featureextractor/__main__.py
@@ -1,5 +1,34 @@
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+import pandas as pd
+import sys
+
 def main():
-    pass
+    input_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    data = pd.read_csv(input_file)
+
+    client = language.LanguageServiceClient()
+
+    sentiments = []
+    for text in data["description"]:
+        sentiments.append(get_data(client, text))
+
+    data["sentiment"] = sentiments
+
+    print data
+    data.to_csv(output_file, index=False)
+
+
+def get_data(client, text):
+    document = types.Document(
+        content = text,
+        type = enums.Document.Type.PLAIN_TEXT)
+
+    return client.analyze_sentiment(document=document).document_sentiment.magnitude
 
 if __name__ == "__main__":
     main()
